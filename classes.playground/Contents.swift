4d394b12f2806b30ea1cec5e6a57f92da6e14980
Simiel <samensah036@st.ug.edu.gh>
2024-05-02 23:07:52 +0000

    week 4

---
+++ classes.playground/Contents.swift
@@ -0,0 +1,16 @@
+class LocalFile{
+    let name: String
+    let fileExtension: String
+    
+    var fullFileName: String{
+        name + "." + fileExtension
+    }
+    
+    init(name: String, fileExtension: String) {
+        self.name = name
+        self.fileExtension = fileExtension
+    }
+}
+
+let file = LocalFile(name: "image", fileExtension: "png")
+print(file.fullFileName)
