eacf8ebe221c45dcd9525d74e29f161500918fb9
Simiel <samensah036@st.ug.edu.gh>
2024-05-04 21:09:13 +0000

    state variable test

---
+++ Little Lemon Text Tutorial/Little Lemon Text Tutorial/Little_Lemon_Text_TutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  Little_Lemon_Text_TutorialApp.swift
+//  Little Lemon Text Tutorial
+//
+//  Created by Samuel Mensa on 04/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct Little_Lemon_Text_TutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Little Lemon Text Tutorial/Little Lemon Text Tutorial/ReservationForm.swift
@@ -0,0 +1,18 @@
+//
+//  ReservationForm.swift
+//  Little Lemon Text Tutorial
+//
+//  Created by Samuel Mensa on 04/05/2024.
+//
+
+import SwiftUI
+
+struct ReservationForm: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ReservationForm()
+}
