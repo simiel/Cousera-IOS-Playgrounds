c71b063dfbdc1974c65f763593fe51fbb50cd978
Simiel <samensah036@st.ug.edu.gh>
2024-05-05 09:02:15 +0000

    alignment, infinity, spacer, padding, frame options, ...

--- Little Lemon Text Tutorial/Little Lemon Text Tutorial/ReservationForm.swift
+++ Little Lemon Text Tutorial/Little Lemon Text Tutorial/ReservationForm.swift
@@ -10,34 +10,38 @@ import SwiftUI
 struct ReservationForm: View {
     @State var customerName = ""
     var body: some View {
-        Form{
-            TextField("Type Your Name", text: $customerName,
-                      onEditingChanged: {status in
-               
-                print(status)
-            }
-            )
-            .onSubmit {
-                print("Text submitted")
-            }
-            .onChange(of: customerName, perform: {newValue in print(newValue)})
-        }
+//        Form{
+//            TextField("Type Your Name", text: $customerName,
+//                      onEditingChanged: {status in
+//               
+//                print(status)
+//            }
+//            )
+//            .onSubmit {
+//                print("Text submitted")
+//            }
+//            .onChange(of: customerName, perform: {newValue in print(newValue)})
+//        }
         ZStack() {
                     Circle()
                         .foregroundColor(Color.gray)
                     Circle()
                         .scale(x: 0.55, y: 0.55)
                         .foregroundColor(Color.red)
-            HStack (alignment: .bottom){
+            VStack (alignment: .leading){
                         Image(systemName: "globe")
                             .imageScale(.large)
                             .foregroundColor(.accentColor)
                         Text ("Hello, world!")
-                        VStack {
+                    .padding()
+                Spacer()
+                HStack(spacing: 50) {
                             Button("One"){}
                             Button("Two"){}
                         }
+//                        .frame(maxHeight: .infinity, alignment: .top)
                     }
+            .frame(maxWidth: .infinity, alignment: .trailing)
                     
                 }
         
