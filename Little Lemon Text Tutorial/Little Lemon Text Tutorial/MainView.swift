539c8c79ee83374381880a4358a2f2307b1a1103
Simiel <samensah036@st.ug.edu.gh>
2024-05-05 09:46:28 +0000

    Controls lesson

---
+++ Little Lemon Text Tutorial/Little Lemon Text Tutorial/MainView.swift
@@ -0,0 +1,18 @@
+//
+//  MainView.swift
+//  Little Lemon Text Tutorial
+//
+//  Created by Samuel Mensa on 05/05/2024.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainView()
+}
