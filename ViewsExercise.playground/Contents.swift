b33ca782c55b63c00aebd94d8410c49ca6a7ef8f
Simiel <samensah036@st.ug.edu.gh>
2024-05-05 11:06:57 +0000

    ...

--- ViewsExercise.playground/Contents.swift
+++ ViewsExercise.playground/Contents.swift
@@ -1,3 +1,24 @@
-import UIKit
+import SwiftUI
+import PlaygroundSupport
 
-var greeting = "Hello, playground"
+struct ParentView: View {
+    @State var reservedDate = Date()
+    var body: some View {
+        HStack{
+            Rectangle()
+                .fill(Color.accentColor)
+                .frame(width: 50, height: 50)
+            
+            Text("Rectangle One")
+                .foregroundColor(.white)
+                .padding()
+                .background(Color.yellow)
+            
+            Rectangle()
+                .fill(Color.blue)
+                .frame(width: 100, height: 30)
+        }
+    }
+}
+
+PlaygroundPage.current.setLiveView(ParentView())
