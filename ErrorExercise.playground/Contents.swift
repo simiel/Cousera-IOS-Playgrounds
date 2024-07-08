7f46109bc86dd0f8d7767f0070e7891b3c04a1b5
Simiel <samensah036@st.ug.edu.gh>
2024-07-08 04:01:30 +0000

    Practice defer-in error handling

--- ErrorExercise.playground/Contents.swift
+++ ErrorExercise.playground/Contents.swift
@@ -1,27 +1,12 @@
-class Calculator{
-    func divide(x: Double, y:Double) throws -> Double {
-        if y == 0{
-            throw CalculatorError.divisionByZero
-        }
-        return x/y
+let file= open(“file.txt”)
+
+
+do {
+    defer {
+        file.close()
     }
+ 
+    let result = try readFile(file)
+} catch {
+    // Handle errors here
 }
-
-enum CalculatorError: Error{
-    case divisionByZero
-}
-
-let calculator = Calculator()
-
-do{
-    let successfulResult2 = try calculator.divide(x: 1, y: 2)
-        print(successfulResult2)
-    
-    let successfulResult = try calculator.divide(x: 100, y: 0)
-    print(successfulResult)
-}
-catch{
-    print("Division by zero detected and not allowed")
-}
-
-
